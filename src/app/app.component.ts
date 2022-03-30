5706136e128893e6bd5fac94b6768bdcc6b6c820
l3miage-bellatim <bellatif@yahoo.fr>
2022-03-30 09:22:15 +0200

    bootstrap + input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { TodolistService } from './todolist.service';
+console.log("test");
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[TodolistService]
 })
 export class AppComponent {
-  title = 'l3m-tpX-todolist-angular-y2022';
+  title = 'l3m-tpX-todolist-angular-y2022';  
+
+  constructor(private TS: TodolistService){
+    
+  }
+
+    create(){
+    // this.TS.create(...arguments);
+    console.log("create");
+    
+  }
 }
